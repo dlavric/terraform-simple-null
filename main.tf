31628a1e51bef1a6d51ca1aeec82652554723bf0
Daniela Lavric <daniela@hashicorp.com>
2024-05-16 16:45:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,6 @@
-resource "null_resource" "null" {}
+resource "null_resource" "test" {}
+resource "null_resource" "test3" {}
+resource "null_resource" "test4" {}
+resource "null_resource" "test5" {}
+resource "null_resource" "test6" {}
+resource "null_resource" "test7" {}
