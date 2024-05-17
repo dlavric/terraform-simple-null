db2771eb35458be53f7ad6e98e19ad161fd3912b
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2024-05-17 11:17:07 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,66 @@
-resource "null_resource" "test" {}
-resource "null_resource" "test3" {}
-resource "null_resource" "test4" {}
-resource "null_resource" "test5" {}
-resource "null_resource" "test6" {}
-resource "null_resource" "test7" {}
-resource "null_resource" "test8" {}
-resource "null_resource" "test9" {}
+variable "mycount" {
+  default = 10
+}
+
+resource "random_pet" "pet1" {
+  prefix = timestamp()
+  length = 1
+}
+
+output "pet1" {
+  value = random_pet.pet1.*.id
+}
+
+resource "null_resource" "null1" {
+  count = var.mycount
+  triggers = {
+    always_run = timestamp()
+  }
+  provisioner "local-exec" {
+    command = "echo ${random_pet.pet1.id}"
+  }
+}
+
+output "null1" {
+  value = null_resource.null1.*.id
+}
+
+resource "null_resource" "null2" {
+  count = var.mycount
+  triggers = {
+    always_run = timestamp()
+  }
+}
+
+output "null2" {
+  value = null_resource.null2.*.id
+}
+
+resource "random_pet" "pet2" {
+  count  = var.mycount
+  prefix = timestamp()
+}
+
+output "pet2" {
+  value = random_pet.pet2.*.id
+}
+
+resource "null_resource" "null3" {
+  count = var.mycount
+  triggers = {
+    always_run = timestamp()
+  }
+}
+
+output "null3" {
+  value = null_resource.null3.*.id
+}
+
+resource "random_pet" "pet3" {
+  count  = var.mycount
+  prefix = timestamp()
+}
+
+output "pet3" {
+  value = random_pet.pet3.*.id
+}
